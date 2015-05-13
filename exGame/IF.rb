9d8b4b789f24c83ff1adaed421e69f49c3cb597c
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-13 18:17:07 +1000

    added module for holding if statements. the runner is getting big, and way outside its juristiction.

---
+++ exGame/IF.rb
@@ -0,0 +1,38 @@
+module IF
+  def IF.is_a_sending_opt?(opts,arg)
+    if arg == ""
+      return false
+    end
+    opts.each do |opt|
+      if opt.include?(arg)
+        return true
+      end
+    end
+    return false
+  end
+
+  def IF.is_info_arg?(arg)
+    info_args = "-v-c-w"
+    if arg == ""
+      return false
+    end
+    return info_args.include?(arg)
+  end
+
+  def self.can_kill?(attacker,defender)
+    return attacker.pronunciation >= defender.rarity
+  end
+
+  def self.can_survive?(defender,attacker)
+    return defender.rarity > attacker.pronunciation
+  end
+
+  def IF.can_kill_and_survive?(killer,defender)
+    return can_kill?(killer,defender) && can_survive?(killer,defender)
+  end
+
+  def IF.will_mutually_destruct?(attacker,defender)
+    return can_kill?(attacker,defender) && can_kill?(defender,attacker)
+  end
+
+end
