538b9b3100385df855b0dd9d71a447b3293034bf
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-20 01:24:14 +1000

    converted the former puts tests to assert tests. they don't really test handling though.

--- tests/test_Word.rb
+++ tests/test_Word.rb
@@ -11,76 +11,76 @@ class TestWord < Test::Unit::TestCase
   def test_Test_word()
     puts "\ttesting...Test-Word"
     test_word = Word.new("test", 'e', 'tst', 1)
-    # puts "created word 'test'"
     tw_spelling = test_word.spelling
-    # puts "you spell the word #{test_word}: #{tw_spelling}"
+    assert_equal("test",tw_spelling)
+
     tw_vowels = test_word.vowels
-    # puts "test contains the vowel: #{tw_vowels}"
+    assert_equal('e',tw_vowels)
+
     tw_cons = test_word.consonants
-    # puts "test contains the cons.: #{tw_cons}"
+    assert_equal('tst',tw_cons)
+
+    # TODO calculate these, so tests can be made
     tw_rar = test_word.rarity
     tw_pro = test_word.pronunciation
     tw_mem = test_word.memory_cost
-    # puts "test has rarity:#{tw_rar}, pronunciation:#{tw_pro}, and memory cost:#{tw_mem}"
     tw_ratio = test_word.ratio
-    # puts "this gives test a ratio of #{tw_ratio}"
-    # puts " "
   end
 
   def test_Vowel_word()
     puts "\ttesting...Vowel-Word"
     vow_word = Word.new("aeiotes", 'aeioe', 'ts', 1)
-    # puts "created vowel word 'aeiotes'"
     vw_spelling = vow_word.spelling
-    # puts "you spell the word #{vow_word}: #{vw_spelling}"
+    assert_equal("aeiotes",vw_spelling)
+
     vw_vowels = vow_word.vowels
-    # puts "aeiotes contains the vowels: #{vw_vowels}"
+    assert_equal('aeioe',vw_vowels)
+
     vw_cons = vow_word.consonants
-    # puts "aeiotes contains the cons.: #{vw_cons}"
+    assert_equal('ts',vw_cons)
+
+    # TODO calculate these, so tests can be made
     vw_rar = vow_word.rarity
     vw_pro = vow_word.pronunciation
     vw_mem = vow_word.memory_cost
-    # puts "aeiotes has rarity:#{vw_rar}, pronunciation:#{vw_pro}, and memory cost:#{vw_mem}"
     vw_ratio = vow_word.ratio
-    # puts "this gives aeiotes a ratio of #{vw_ratio}"
-    # puts " "
   end
 
   def test_cons_word()
     puts "\ttesting...Consonant-Word"
     cons_word = Word.new("trackback", 'aa', 'trckbck', 2)
-    # puts "created consonant word 'trackback'"
     cw_spelling = cons_word.spelling
-    # puts "you spell the word #{cons_word}: #{cw_spelling}"
+    assert_equal("trackback",cw_spelling)
+
     cw_vowels = cons_word.vowels
-    # puts "trackback contains the vowels: #{cw_vowels}"
+    assert_equal('aa',cw_vowels)
+
     cw_cons = cons_word.consonants
-    # puts "trackback contains the cons.: #{cw_cons}"
+    assert_equal('trckbck',cw_cons)
+
+    # TODO calculate these, so tests can be made
     cw_rar = cons_word.rarity
     cw_pro = cons_word.pronunciation
     cw_mem = cons_word.memory_cost
-    # puts "trackback has rarity:#{cw_rar}, pronunciation:#{cw_pro}, and memory cost:#{cw_mem}"
     cw_ratio = cons_word.ratio
-    # puts "this gives trackback a ratio of #{cw_ratio}"
-    # puts " "
   end
 
   def test_balanced_word()
     puts "\ttesting...Balanced-Word"
     bal_word = Word.new("atesitob", 'aeio', 'tstb', 4)
-    # puts "created balanced word 'atesitob'"
     bw_spelling = bal_word.spelling
-    # puts "you spell the word #{bal_word}: #{bw_spelling}"
+    assert_equal("atesitob",bw_spelling)
+
     bw_vowels = bal_word.vowels
-    # puts "atesitob contains the vowels: #{bw_vowels}"
+    assert_equal('aeio',bw_vowels)
+
     bw_cons = bal_word.consonants
-    # puts "atesitob contains the cons.: #{bw_cons}"
+    assert_equal('tstb',bw_cons)
+
+    # TODO calculate these, so tests can be made
     bw_rar = bal_word.rarity
     bw_pro = bal_word.pronunciation
     bw_mem = bal_word.memory_cost
-    # puts "atesitob has rarity:#{bw_rar}, pronunciation:#{bw_pro}, and memory cost:#{bw_mem}"
     bw_ratio = bal_word.ratio
-    # puts "this gives atesitob a ratio of #{bw_ratio}"
-    # puts " "
   end
 end
