2293d6ad5a818a59102ba0f6ce8f3c334894a02d
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-20 16:00:16 +1000

    the tests for Word is complete.

--- tests/test_Word.rb
+++ tests/test_Word.rb
@@ -13,13 +13,14 @@ class TestWord < Test::Unit::TestCase
     # STEPS OF RARITY TEST
 
     # test with no vowels
-    rarity_fun = ((@length * 2.43 + @index_coefficient).round 2)
-    rarity_calc = word.calc_attrib 'rar'
+    test_word = Word.new(word.spelling, '', word.consonants, index)
+    rarity_fun = ((test_word.spelling.length * 2.43 + (index * 0.25)).round 2)
+    rarity_calc = test_word.calc_attrib 'rar'
     assert_equal(rarity_fun, rarity_calc)
-    assert_equal(rarity_calc, word.rarity)
+    assert_equal(rarity_calc, test_word.rarity)
 
     # test with vowels
-    rarity_fun = (((@length * (2.7 / @vowels.length)) * 0.9 + @index_coefficient).round 2)
+    rarity_fun = (((word.spelling.length * (2.7 / word.vowels.length)) * 0.9 + (index * 0.25)).round 2)
     rarity_calc = word.calc_attrib 'rar'
     assert_equal(rarity_fun, rarity_calc)
     assert_equal(rarity_calc, word.rarity)
@@ -30,13 +31,14 @@ class TestWord < Test::Unit::TestCase
     # STEPS OF PRONUNCIATION TEST
 
     # test with no consonants
-    pronunciation_fun = ((@length * 1.74 + @index_coefficient).round 2)
-    pronunciation_calc = word.calc_attrib 'pro'
+    test_word = Word.new(word.spelling, word.vowels, '', index)
+    pronunciation_fun = ((test_word.spelling.length * 1.74 + (index * 0.25)).round 2)
+    pronunciation_calc = test_word.calc_attrib 'pro'
     assert_equal(pronunciation_fun, pronunciation_calc)
-    assert_equal(pronunciation_calc, word.pronunciation)
+    assert_equal(pronunciation_calc, test_word.pronunciation)
 
     # test with consonants
-    pronunciation_fun = (((@length * (2.9 / @consonants.length)) * 0.6 + @index_coefficient).round 2)
+    pronunciation_fun = (((word.spelling.length * (2.9 / word.consonants.length)) * 0.6 + (index * 0.25)).round 2)
     pronunciation_calc = word.calc_attrib 'pro'
     assert_equal(pronunciation_fun, pronunciation_calc)
     assert_equal(pronunciation_calc, word.pronunciation)
@@ -46,7 +48,7 @@ class TestWord < Test::Unit::TestCase
   def innertest_memory_cost(word, index)
     # STEPS OF MEMORY COST TEST
     # TODO MAKE TEST CHECK HANDLING
-    mem_fun = ((1.47 * @length + @index_coefficient).round 2)
+    mem_fun = ((1.47 * word.spelling.length + (index * 0.25)).round 2)
     mem_calc = word.calc_attrib 'mem'
     assert_equal(mem_fun, mem_calc)
     assert_equal(mem_calc, word.memory_cost)
@@ -55,7 +57,8 @@ class TestWord < Test::Unit::TestCase
 
   def test_Test_word()
     puts "\ttesting...Test-Word"
-    test_word = Word.new("test", 'e', 'tst', 1)
+    index = 1
+    test_word = Word.new("test", 'e', 'tst', index)
     tw_spelling = test_word.spelling
     assert_equal("test", tw_spelling)
 
@@ -65,16 +68,17 @@ class TestWord < Test::Unit::TestCase
     tw_cons = test_word.consonants
     assert_equal('tst', tw_cons)
 
-    # TODO calculate these, so tests can be made
-    tw_rar = test_word.rarity
-    tw_pro = test_word.pronunciation
-    tw_mem = test_word.memory_cost
-    tw_ratio = test_word.ratio
+    tw_rar = innertest_rarity(test_word, index)
+    tw_pro = innertest_pronunciation(test_word, index)
+    tw_mem = innertest_memory_cost(test_word, index)
+    tw_ratio = (tw_mem / (tw_rar + tw_pro)).round 2
+    assert_equal(tw_ratio,test_word.ratio)
   end
 
   def test_Vowel_word()
     puts "\ttesting...Vowel-Word"
-    vow_word = Word.new("aeiotes", 'aeioe', 'ts', 1)
+    index = 1
+    vow_word = Word.new("aeiotes", 'aeioe', 'ts', index)
     vw_spelling = vow_word.spelling
     assert_equal("aeiotes", vw_spelling)
 
@@ -84,16 +88,17 @@ class TestWord < Test::Unit::TestCase
     vw_cons = vow_word.consonants
     assert_equal('ts', vw_cons)
 
-    # TODO calculate these, so tests can be made
-    vw_rar = vow_word.rarity
-    vw_pro = vow_word.pronunciation
-    vw_mem = vow_word.memory_cost
-    vw_ratio = vow_word.ratio
+    vw_rar = innertest_rarity(vow_word, index)
+    vw_pro = innertest_pronunciation(vow_word, index)
+    vw_mem = innertest_memory_cost(vow_word, index)
+    vw_ratio = (vw_mem / (vw_rar + vw_pro)).round 2
+    assert_equal(vw_ratio,vow_word.ratio)
   end
 
   def test_cons_word()
     puts "\ttesting...Consonant-Word"
-    cons_word = Word.new("trackback", 'aa', 'trckbck', 2)
+    index = 2
+    cons_word = Word.new("trackback", 'aa', 'trckbck', index)
     cw_spelling = cons_word.spelling
     assert_equal("trackback", cw_spelling)
 
@@ -103,16 +108,17 @@ class TestWord < Test::Unit::TestCase
     cw_cons = cons_word.consonants
     assert_equal('trckbck', cw_cons)
 
-    # TODO calculate these, so tests can be made
-    cw_rar = cons_word.rarity
-    cw_pro = cons_word.pronunciation
-    cw_mem = cons_word.memory_cost
-    cw_ratio = cons_word.ratio
+    cw_rar = innertest_rarity(cons_word, index)
+    cw_pro = innertest_pronunciation(cons_word, index)
+    cw_mem = innertest_memory_cost(cons_word, index)
+    cw_ratio = (cw_mem / (cw_rar + cw_pro)).round 2
+    assert_equal(cw_ratio,cons_word.ratio)
   end
 
   def test_balanced_word()
     puts "\ttesting...Balanced-Word"
-    bal_word = Word.new("atesitob", 'aeio', 'tstb', 4)
+    index = 4
+    bal_word = Word.new("atesitob", 'aeio', 'tstb', index)
     bw_spelling = bal_word.spelling
     assert_equal("atesitob", bw_spelling)
 
@@ -122,10 +128,10 @@ class TestWord < Test::Unit::TestCase
     bw_cons = bal_word.consonants
     assert_equal('tstb', bw_cons)
 
-    # TODO calculate these, so tests can be made
-    bw_rar = bal_word.rarity
-    bw_pro = bal_word.pronunciation
-    bw_mem = bal_word.memory_cost
-    bw_ratio = bal_word.ratio
+    bw_rar = innertest_rarity(bal_word, index)
+    bw_pro = innertest_pronunciation(bal_word, index)
+    bw_mem = innertest_memory_cost(bal_word, index)
+    bw_ratio = (bw_mem / (bw_rar + bw_pro)).round 2
+    assert_equal(bw_ratio,bal_word.ratio)
   end
 end
